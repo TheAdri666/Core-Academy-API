908179ba6100cff9b196dd1ee0708f152d2a3fd6
TheAdri666 <adrianinfante66@gmail.com>
2022-03-23 20:48:56 +0100

    Added routers to app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const userRouter = require('./routes/userRouter');
+const diskRouter = require('./routes/diskRouter');
 
 const app = express();
 
 app.use(express.json());
+app.use('/user', userRouter);
+app.use('disk', diskRouter);
 
 module.exports = app;
